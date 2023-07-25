de93e14df834ece7a0c285d315f355e84046ad15
Prajwal Deshmukh <smartpd2607@gmail.com>
2023-07-25 12:26:22 +0530

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,145 @@
-import Image from 'next/image'
+"use client"
+import React, { useState } from 'react';
 
 export default function Home() {
+  const [coverLetter, setCoverLetter] = useState('');
+  const [isLoading, setIsLoading] = useState(false);
+  const [uploadedFileName, setUploadedFileName] = useState('');
+  const [extractedContent, setExtractedContent] = useState('');
+
+  const handleFileUpload = async (event) => {
+    const file = event.target.files?.[0];
+    if (file) {
+      setUploadedFileName(file.name);
+      setIsLoading(true);
+
+      const formData = new FormData();
+      formData.append('file', file);
+      formData.append('output_format', 'text');
+
+      try {
+        const response = await fetch('https://api.worqhat.com/api/ai/v2/pdf-extract', {
+          method: 'POST',
+          headers: {
+            'x-api-key': 'U2FsdGVkX187FPQxzgbmIVjXh3O1+xyor30KWVrIBMuFEqGv8NfzXPjE53e3Ju+T',
+            'x-org-key': 'U2FsdGVkX19lq3bhhF5TRouMiyL2HvEBD2V5j5nNl6dNL9JWPbsXW0rqlzssW8GieFki6oRVDKTb/z01Hc7m+Q==',
+          },
+          body: formData,
+        });
+
+        const data = await response.json();
+        console.log(data);
+
+        // Check if the content property exists in the API response
+        if (data.content) {
+          const extractedContent = data.content;
+          setExtractedContent(extractedContent);
+        } else {
+          console.error('Error extracting content: Unexpected API response format');
+        }
+      } catch (error) {
+        console.error('Error extracting content:', error);
+      } finally {
+        setIsLoading(false);
+      }
+    }
+  };
+
+  const generateCoverLetter = async () => {
+    setIsLoading(true);
+    try {
+      const question = 'Generated a cover letter for the given extracted content';
+      const fullQuestion = `${extractedContent}\n\n${question}`;
+      const requestData = {
+        question: fullQuestion,
+        randomness: 0.4, // You can adjust the randomness factor as needed
+      };
+
+      const response = await fetch('https://api.worqhat.com/api/ai/content/v2', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json',
+          'x-api-key': 'U2FsdGVkX187FPQxzgbmIVjXh3O1+xyor30KWVrIBMuFEqGv8NfzXPjE53e3Ju+T',
+          'x-org-key': 'U2FsdGVkX19lq3bhhF5TRouMiyL2HvEBD2V5j5nNl6dNL9JWPbsXW0rqlzssW8GieFki6oRVDKTb/z01Hc7m+Q==',
+        },
+        body: JSON.stringify(requestData),
+      });
+
+      if (!response.ok) {
+        throw new Error('Network response was not ok');
+      }
+
+      const data = await response.json();
+      console.log(data); // Optional: Log the API response data
+
+      // Check if the content property exists in the API response
+      if (data.content) {
+        const generatedCoverLetter = data.content;
+        setCoverLetter(generatedCoverLetter);
+      } else {
+        console.error('Error generating cover letter: Unexpected API response format');
+      }
+    } catch (error) {
+      console.error('Error generating cover letter:', error);
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{' '}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
+    <main className="flex flex-col h-full items-center justify-center p-5">
+      <div className="container h-4/5 my-1 bg-white rounded-md text-white p-10">
+        <h1 className="my-3 font-sans text-center text-4xl font-bold p-3 drop-shadow-lg bg-red-600">
+          Resume to cover letter
+        </h1>
+
+        <div className="text-center container rounded-md text-white my-4 p-5 flex flex-col items-center justify-center">
+          <div className="upload flex flex-col items-center">
+            <div>
+              <label
+                htmlFor="upload-button"
+                className="btn bg-blue-600 text-white p-3 mx-auto cursor-pointer rounded-md"
+              >
+                Upload Resume
+                <input
+                  type="file"
+                  id="upload-button"
+                  className="hidden bg-bluem w-64"
+                  accept=".pdf,.doc,.docx"
+                  onChange={handleFileUpload}
+                />
+              </label>
+              <div className="m-2 p-2 text-black">
+                {uploadedFileName && <p>Uploaded File: {uploadedFileName}</p>}
+              </div>
+            </div>
+            {extractedContent && (
+              <div>
+                <button
+                  type="button"
+                  id="generate-button"
+                  className="btn bg-green-600 text-white px-4 py-2 m-1 w-72 mx-auto cursor-pointer rounded-md"
+                  disabled={!uploadedFileName || isLoading}
+                  onClick={generateCoverLetter}
+                >
+                  {isLoading ? 'Generating...' : 'Generate cover letter'}
+                </button>
+              </div>
+            )}
+          </div>
+        </div>
+        <div className="cv bg-white h-64 rounded-md text-white p-5">
+          <textarea
+            className="border-2 text-black rounded-md w-full p-4 text-center drop-shadow-xl"
+            id=""
+            cols="30"
+            rows="8"
+            placeholder="Welcome to Resume to Cover Letter by Worqhat! Upload your resume and generate your cover letter."
+            value={coverLetter}
+            readOnly
+          ></textarea>
         </div>
       </div>
-
-      <div className="relative flex place-items-center before:absolute before:h-[300px] before:w-[480px] before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-[240px] after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
     </main>
-  )
+  );
 }
